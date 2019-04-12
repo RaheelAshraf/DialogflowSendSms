44a505ddb1fdf62d74024d02a4b2a686f807ca61
iamraheelashraf <labkoat@gmail.com>
2019-04-13 04:16:23 +0500

    key added

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { WebhookClient } = require('dialogflow-fulfillment');
+const Nexmo = require('nexmo');
+const socketio = require('socket.io');
 const app = express().use(bodyParser.json());
 const port = process.env.PORT || 8080;
+
+// Init Nexmo
+const nexmo = new Nexmo({
+    apiKey: '578d520e',
+    apiSecret: '8V25q2Za82CXMzT2'
+}, { debug: true });
 
 app.post('/webhook', (request, response) => {
 
@@ -18,8 +26,31 @@ app.post('/webhook', (request, response) => {
 
     function sendmessage(agent) {
         const number = agent.parameters.number;
-        const message = agent.parameters.message;
-        agent.add(`${number}, ${message}`);
+        const text = agent.parameters.message;
+        nexmo.message.sendSms(
+            'YOURVURTUALNUMBER', number, text, { type: 'unicode' },
+            (err, responseData) => {
+                if (err) {
+                    console.log(err);
+                    agent.add(err); 
+                } else {
+                    const { messages } = responseData;
+                    const { ['message-id']: id, ['to']: number, ['error-text']: error } = messages[0];
+                    console.dir(responseData);
+                    agent.add(responseData);
+                    // Get data from response
+                    const data = {
+                        id,
+                        number,
+                        error
+                    };
+
+                    // Emit to the client
+                    io.emit('smsStatus', data);
+                }
+            }
+        );
+
     }
 
     let intentMap = new Map();
@@ -29,8 +60,14 @@ app.post('/webhook', (request, response) => {
     _agent.handleRequest(intentMap);
 })
 
+// Start server
+const server = app.listen(port, () => console.log(`Server started on port ${port}`));
 
-app.listen(port, (req, res) => {
-
-    console.log(`app started on port: ${port}`)
-})
+// Connect to socket.io
+const io = socketio(server);
+io.on('connection', (socket) => {
+  console.log('Connected');
+  io.on('disconnect', () => {
+    console.log('Disconnected');
+  })
+});
